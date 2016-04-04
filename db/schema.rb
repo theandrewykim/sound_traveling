03920464d7b5057d44be785fff6c48a4d914dfc1
will van wart <william@wvwproductions.com>
2016-04-04 14:24:29 -0400

    change consumer_key to api_key  naming_issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160403221711) do
+ActiveRecord::Schema.define(version: 20160404161119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
