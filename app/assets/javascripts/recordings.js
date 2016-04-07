6b5652cba45c2f883a0ab514264dfd6ccdc5bef9
Andrew Kim <email": null>
2016-04-07 16:33:05 -0400

    fix player and ajax clicks

--- app/assets/javascripts/recordings.js
+++ app/assets/javascripts/recordings.js
@@ -2,44 +2,6 @@
 // All this logic will automatically be available in application.js.
 jQuery(document).ready(function($) {
 
-  var data = $('.recording_params').data();
-
-
-  var playerParams = {
-        title: data.recordingname,
-        mp3: data.url
-      }
-
-  $("#jquery_jplayer_audio_1").jPlayer({
-
-    ready: function(event) {
-      $(this).jPlayer("setMedia", playerParams );
-    },
-    play: function() { // Avoid multiple jPlayers playing together.
-      $(this).jPlayer("pauseOthers");
-    },
-    timeFormat: {
-      padMin: false
-    },
-    swfPath: "js",
-    supplied: "mp3",
-    cssSelectorAncestor: "#jp_container_audio_1",
-    useStateClassSkin: true,
-    autoBlur: false,
-    smoothPlayBar: true,
-    remainingDuration: true,
-    keyEnabled: true,
-    keyBindings: {
-      // Disable some of the default key controls
-      loop: null,
-      muted: null,
-      volumeUp: null,
-      volumeDown: null
-    },
-    wmode: "window"
-  });
-
-
 
 
 $("#dropdown").on("submit",function(e){
@@ -80,11 +42,12 @@ else {
 
 
 
-$(".container").on("click",".button_to", function(e){
+$("#follow-container").on("click",".button_to", function(e){
   e.preventDefault()
 
+  var $data = $(".recording").data().recordings.user_id
   var $url = $(this).attr("action")
-  if($url == "/users/4/relationships"){
+  if($url == "/users/"+$data+"/relationships"){
     var type = "POST"
   }
   else{
